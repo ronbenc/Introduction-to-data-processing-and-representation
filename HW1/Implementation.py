223d3b047de4c60524c5e54915956e470aca1105
t-ronb <t-ronb@microsoft.com>
2021-11-29 15:15:37 +0200

    a

--- HW1/Implementation.py
+++ HW1/Implementation.py
@@ -1,7 +1,7 @@
 from matplotlib import pyplot as plt
 import numpy as np
 
-def uniform_quantize_function(low,delta,x):
+def uniform_quantize_function(low, delta, x):
     return low+((np.floor(((x-low)/delta))+(1/2))*delta)
 
 def uniform_quantize(img,num_bits,low,high):
@@ -9,7 +9,9 @@ def uniform_quantize(img,num_bits,low,high):
     delta = (high-low)/k
     return uniform_quantize_function(low,delta,img)
 
-
+def compute_MSE(orig_img, q_img):
+    N = orig_img.ravel().shape[0]
+    return (np.power((orig_img-q_img).astype(float), 2)).sum()/N
 
 
 if __name__ == '__main__':
@@ -17,10 +19,17 @@ if __name__ == '__main__':
     img = plt.imread(img_path)
     grayscale_img = img[:,:,0]
     plt.hist(grayscale_img.ravel(),bins=256,range=[0,256])
+    plt.show()
     max_num_bits = 8
     
-    for bits in range(1,max_num_bits+1):
-        quantized_img = uniform_quantize(grayscale_img,bits,0,256)
-        plt.imshow(quantized_img,'gray')
-        plt.title('number of bits = {}'.format(bits))
-        plt.show()
+    b_list = [bits for bits in range(1,max_num_bits+1)]
+    q_list = []
+
+
+    for bit in b_list:
+        quantized_img = uniform_quantize(grayscale_img,bit,0,256)
+        q_list.append(compute_MSE(grayscale_img, quantized_img))
+
+    plt.plot(b_list, q_list)
+    plt.show()
+        
