640705d0ae47f6c39de06d501b8e0b2489e953f4
nitzanleshem <nitzanleshem@gmail.com>
2021-11-29 16:26:34 +0200

    Finished 1 parts 1 and 2

--- HW1/Implementation.py
+++ HW1/Implementation.py
@@ -8,11 +8,36 @@ def uniform_quantize(img,num_bits,low,high):
     k = 2**num_bits
     delta = (high-low)/k
     return uniform_quantize_function(low,delta,img)
+    # return (get_decisions_and_representations(delta,k,low),uniform_quantize_function(low,delta,img))
 
 def compute_MSE(orig_img, q_img):
     N = orig_img.ravel().shape[0]
     return (np.power((orig_img-q_img).astype(float), 2)).sum()/N
 
+def get_decisions_and_representations(delta,k,low):
+    decisions = [low+i*delta for i in range(0,k+1)]
+    representations = [low + (i-(1/2))*delta for i in range(1,k+1)]
+    return (decisions,representations)
+
+def plot_reps_and_dec(b_list):
+    x = [i for i in range(0,256)]
+    for bit in b_list:
+        plt.plot(x,uniform_quantize(np.array(x),bit,0,256))
+        plt.show()
+
+def plot_MSE(b_list):
+    q_list = []
+
+    for bit in b_list:
+        quantized_img = uniform_quantize(grayscale_img,bit,0,256)
+        q_list.append(compute_MSE(grayscale_img, quantized_img))
+        
+        # decisions.append(dec_and_reps[0])
+        # representations.append(dec_and_reps[1])
+        # plt.imshow(quantized_img,'gray')
+        # plt.show()
+    plt.plot(b_list, q_list)
+    plt.show()
 
 if __name__ == '__main__':
     img_path = './HW1/lena_gray.gif'
@@ -21,15 +46,19 @@ if __name__ == '__main__':
     plt.hist(grayscale_img.ravel(),bins=256,range=[0,256])
     plt.show()
     max_num_bits = 8
-    
+
     b_list = [bits for bits in range(1,max_num_bits+1)]
-    q_list = []
+
+    #2.1
+    plot_MSE(b_list)
+
+    #2.b
+    plot_reps_and_dec(b_list)
+
+    
+   
 
 
-    for bit in b_list:
-        quantized_img = uniform_quantize(grayscale_img,bit,0,256)
-        q_list.append(compute_MSE(grayscale_img, quantized_img))
 
-    plt.plot(b_list, q_list)
-    plt.show()
-        
+
+    
