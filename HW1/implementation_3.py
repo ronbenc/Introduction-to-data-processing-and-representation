ec92f76899e3df0fa4c4143af54fdbab9d74b977
nitzanleshem <nitzanleshem@gmail.com>
2021-12-03 17:35:16 +0200

    Finished IRLS

--- HW1/implementation_3.py
+++ HW1/implementation_3.py
@@ -2,15 +2,16 @@ from matplotlib import pyplot as plt
 import numpy as np
 
 class IRLS:
-    def __init__(self, f, N, epsilon, p = 1) -> None:
+    def __init__(self, f, N, epsilon, p = 1,delta =0.0000001) -> None:
         self.f = f
         self.N = N
         self.epsilon = epsilon
         self.p = p
-        self.w = np.eye(f.shape)
+        self.w = np.ones(f.shape)
         self.g = np.zeros_like(f) # f^
-        self.I = f.shape[0]/N
+        self.I = int(f.shape[0]/N)
         self.error = None
+        self.delta = delta
     
 
     def workflow(self):
@@ -19,7 +20,8 @@ class IRLS:
             self.w = self.get_updated_w()
             prev_error = self.error
             self.error = self.calc_error()
-            if(prev_error is not None and (prev_error-self.error) < self.delta):
+            print('old error = {} new error = {}'.format(prev_error, self.error))
+            if(prev_error is not None and np.abs((prev_error-self.error)) < self.delta):
                 break
 
     def get_updated_g(self):
@@ -27,23 +29,36 @@ class IRLS:
 
         for row in range(self.N):
             for col in range(self.N):
-                new_g[row*self.I:(row*self.I+1),col*self.I:col(*self.I+1)] = self.get_updated_g_i(row, col)
+                new_g[int(row*self.I):int(((row+1)*(self.I))),int(col*self.I):int((col+1)*(self.I))] = self.get_updated_g_i(row, col)
         
         return new_g
 
     def get_updated_g_i(self, row, col) -> float:
-        w_i = self.w[row*self.I:(row*self.I+1),col*self.I:col(*self.I+1)]
-        f_i = self.f[row*self.I:(row*self.I+1),col*self.I:col(*self.I+1)]
+        start_row = int(row*self.I)
+        end_row = int((row+1)*(self.I))
+        start_column = int(col*self.I)
+        end_column = int((col+1)*(self.I))
+        w_i = self.w[start_row:end_row,start_column:end_column]
+        f_i = self.f[start_row:end_row,start_column:end_column]
 
         return (w_i * f_i).sum()/w_i.sum()
 
     def get_updated_w(self):
-        normalized_w = (self.f - self.g)**(self.p-2)
+        normalized_w = np.abs((self.f - self.g))**(self.p-2)
         normalized_w[normalized_w > (1/self.epsilon)] = 1/self.epsilon
         return normalized_w
 
     def calc_error(self):
-        ...
+        return ((np.abs((self.f - self.g))**(self.p)).sum())/len(self.f.ravel())
 
 if __name__ == '__main__':
-    ...
+    img_path = './HW1/lena_gray.gif'
+    img = plt.imread(img_path)
+    grayscale_img = img[:,:,0]
+
+    signal = grayscale_img/255
+
+    irls = IRLS(signal,(2**4),10000,p=4)
+    irls.workflow()
+    plt.imshow(irls.g,'gray',vmin=0,vmax=1)
+    plt.show()
