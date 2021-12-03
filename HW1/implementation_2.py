c5480820e73e0650dfc1d229d08afa1ab3fe2509
nitzanleshem <nitzanleshem@gmail.com>
2021-12-03 11:33:37 +0200

    Added compute and subsample for MAD and MSE

--- HW1/implementation_2.py
+++ HW1/implementation_2.py
@@ -6,14 +6,28 @@ class SubSampler:
     def __init__(self, img, d) -> None:
         self.img = img
         self.d = d
-        self.subsampled_img = None
+        self.img_shape = img.shape
+        self.subsampled_shape = tuple([int(i/d) for i in self.img_shape])
+        self.subsampled_img = np.zeros(self.subsampled_shape)
+        self.error = 0
+
+    
 
     @abstractmethod
     def compute(self):
         ...
     
     def subsample(self):
-        ...
+        self.error = 0
+        
+        for row in range(self.subsampled_shape[0]):
+            for column in range(self.subsampled_shape[1]):
+                error , value = self.compute(self.img[self.d*row:self.d*(row+1),self.d*column:self.d*(column+1)])
+                self.subsampled_img[row,column] = value
+                self.error = self.error + error
+
+        self.error = self.error/len(self.img.ravel())
+        return self.subsampled_img
     
 
     def reconstruct(self):
@@ -24,9 +38,19 @@ class MSESubSampler(SubSampler):
     def __init__(self, img, d) -> None:
         super().__init__(img, d)
 
-    def compute(self):
+    def compute(self,sub_img):
+        mean = int(sub_img.mean())
+        error_sum = (sub_img-mean).sum()
+        return error_sum,mean
 
+class MADSubSampler(SubSampler):
+    def __init__(self, img, d) -> None:
+        super().__init__(img, d)
 
+    def compute(self,sub_img):
+        median = int(np.median(sub_img))
+        error_sum = (sub_img-median).sum()
+        return error_sum,median
 
 if __name__ == '__main__':
     img_path = './HW1/lena_gray.gif'
