87379dcb33e43e08f515e5b3776239a66cbc90b8
t-ronb <t-ronb@microsoft.com>
2022-01-02 18:24:47 +0200

    Create HW3

--- HW2/implementation_2.py
+++ HW2/implementation_2.py
@@ -1,5 +1,6 @@
 from abc import abstractmethod
 import math
+from typing import List
 import numpy as np
 from matplotlib import pyplot as plt
 
@@ -8,15 +9,6 @@ class Matrix:
         self.n = n
         self.size = 2**n
         self.matrix = np.empty((self.size, self.size))
-    
-    def plot_columns(self):
-        x_axis = [x/self.size for x in range(0, self.size+1)]
-        fig,axs = plt.subplots(self.size)
-        tmp_mat = np.vstack((self.matrix[0, :], self.matrix)) #TODO resove this more elegantly
-        for i in range(self.size):
-            axs[i].step(x_axis, tmp_mat[:, i]) #how to plot with bars
-        
-        plt.show()
         
 class Hadamard(Matrix):
     def __init__(self, n: int) -> None:
@@ -74,9 +66,9 @@ class Haar(Matrix):
 
         else:
             prev_haar = Haar(n-1)
-            upper_haar = (1/np.sqrt(2))*np.kron(prev_haar.matrix, np.array([1, 1]))
-            lower_haar = (1/np.sqrt(2))*np.kron(np.eye(prev_haar.size), np.array([1, -1]))
-            self.matrix = np.vstack((upper_haar, lower_haar)).T
+            left_haar = (1/np.sqrt(2))*np.kron(prev_haar.matrix, np.array(([1],[1])))
+            right_haar = (1/np.sqrt(2))*np.kron(np.eye(prev_haar.size), np.array(([1],[-1])))
+            self.matrix = np.hstack((left_haar, right_haar))
 
 class Func:
     def __init__(self, range) -> None:
@@ -113,6 +105,7 @@ class KTermApproximator:
         self.coeff = np.empty((N))
         self.sorted_coeff = np.empty((N))
         self.k_coeff = None
+        self.reconstructed_phi = None
         self._calc_std_coefficients()
         self._calc_all_coefficients()
         self._sort_all_coefficients()
@@ -122,7 +115,8 @@ class KTermApproximator:
         self._choose_k_coefficients(k)
 
     def calc_MSE(self) -> float:
-        coefficients_error = np.sum(np.power(self.k_coeff, 2))
+        clean_k_coeff = [coeff for _,coeff in self.k_coeff]
+        coefficients_error = np.sum(np.power(clean_k_coeff, 2))
         return self.phi.integral_error - coefficients_error
 
     def _calc_std_coefficients(self):
@@ -136,34 +130,138 @@ class KTermApproximator:
 
     def _calc_all_coefficients(self):
         self.coeff = np.matmul(self.basis, self.std_basis_coeff.T)
+        self.coeff = [i for i in enumerate(self.coeff)]
     
     def _sort_all_coefficients(self):
-        self.sorted_coeff = sorted(self.coeff, key=abs, reverse=True)
+        f = lambda x : abs(x[1])
+        self.sorted_coeff = sorted(self.coeff, key=f, reverse=True)
 
     def _choose_k_coefficients(self, k: int):
         self.k_coeff = self.sorted_coeff[:k]
+
+    def reconstruct_phi(self):
+        self.reconstructed_phi = np.zeros(self.N)
+
+        for index, coeff in self.k_coeff:
+            self.reconstructed_phi += coeff*self.basis[index,: ]
 
 
 def create_basis(matrix: Matrix, sum_range: float, N: int):
     return matrix.matrix.T*np.sqrt(N/sum_range)
 
+def plot_columns(matrix, N: int, mat_type = None):
+    x_axis = [x/N for x in range(0, N+1)]
+    if N <= 8:
+        fig,axs = plt.subplots(N)
+        tmp_mat = np.vstack((matrix[0, :], matrix)) #TODO solve this more elegantly
+        for i in range(N):
+            axs[i].step(x_axis, tmp_mat[:, i])
+    
+    else:
+        k = int(N/8)
+        fig,axs = plt.subplots(int(N/k), k)
+        tmp_mat = np.vstack((matrix[0, :], matrix)) #TODO solve this more elegantly
+        for i in range(N):
+            axs[int(i/k), i%k].step(x_axis, tmp_mat[:, i])
 
+    
+    if mat_type == None:
+        plt.suptitle("n = {}".format(int(np.log2(N))))
+
+    else:
+        plt.suptitle("{}, n = {}".format(mat_type, int(np.log2(N))))
+
+    plt.show()
+
+def plot_MSE(k_term_aprox: KTermApproximator, ks: list, basis_name):
+    mse_list = []
+    for k in ks:
+        k_term_aprox.approximate(k)
+
+        mse_list.append(k_term_aprox.calc_MSE())
+
+    print(mse_list)
+
+    plt.plot(ks, mse_list)
+    plt.title(basis_name)
+    plt.xlabel("k")
+    plt.ylabel("MSE")
+    plt.show()
+
+def plot_recontructed_phi(k_term_aprox: KTermApproximator, ks: list, basis_name):
+    x_axis = np.linspace(k_term_aprox.phi.range[0], k_term_aprox.phi.range[1], 5)
+    for k in ks:
+        k_term_aprox.approximate(k)
+        k_term_aprox.reconstruct_phi()
+        reconstructed_phi = [k_term_aprox.reconstructed_phi[0]]
+        reconstructed_phi.extend(k_term_aprox.reconstructed_phi)
+        print(reconstructed_phi)
+        plt.step(x_axis, reconstructed_phi,where='pre')
+        plt.title(basis_name + ", k = {}".format(k))
+        plt.show()
+        
 
 
 if __name__ == '__main__':
-    hadamard = Hadamard(2)
-    hadamard.plot_columns()
-    print(hadamard.matrix)
-    # walsh_hadamard = WalshHadamard(hadamard)
-    # walsh_hadamard.plot_columns()
-    # print(walsh_hadamard.matrix)
-    haar = Haar(2)
-    # haar.plot_columns()
+    # # plot hadamards
+    # for n in [2, 3, 4, 5, 6]:
+    #     hadamard = Hadamard(n)
+    #     N = hadamard.size
+    #     basis = create_basis(hadamard, 1, N)
+    #     plot_columns(basis, N, "Hadamard")
+
+    # # plot walsh-hadamards
+    # for n in [2, 3, 4, 5, 6]:
+    #     hadamard = Hadamard(n)
+    #     walsh_hadamard = WalshHadamard(hadamard)
+    #     N = walsh_hadamard.size
+    #     basis = create_basis(walsh_hadamard, 1, N)
+    #     plot_columns(basis, N, "Walsh-Hadamard")
+
+
+    # # plot haars
+    # for n in [2, 3, 4, 5, 6]:
+    #     haar = Haar(n)
+    #     haar.matrix = haar.matrix.T
+    #     N = haar.size
+    #     basis = create_basis(haar, 1, N)
+    #     plot_columns(basis, N, "Haar")
+
+
     phi = Phi((-4, 5))
     phi_pre = PhiPre((-4, 5))
-    basis = create_basis(Hadamard(2), phi.range[1]-phi.range[0], 4)
-    k_term_aprox = KTermApproximator(phi, phi_pre, basis, 4)
-    for k in range(1, 5):
-        k_term_aprox.approximate(k)
-        print(k_term_aprox.k_coeff)
-        print(k_term_aprox.calc_MSE())
+    ks = [1, 2, 3, 4]
+    func_range = 9
+
+    # eye = Eye(2)
+    # standard_basis = create_basis(eye, func_range, 4)
+    # k_term_aprox = KTermApproximator(phi, phi_pre, standard_basis, 4)
+    # k_term_aprox.approximate(4)
+    # k_term_aprox.reconstruct_phi()
+    # plot_recontructed_phi(k_term_aprox, ks, "Standard basis")
+    # plot_MSE(k_term_aprox, ks, "Standard basis")
+
+    # hadamard = Hadamard(2)
+    # hadamarad_basis = create_basis(hadamard, func_range, 4)
+    # k_term_aprox = KTermApproximator(phi, phi_pre, hadamarad_basis, 4)
+    # k_term_aprox.approximate(4)
+    # k_term_aprox.reconstruct_phi()
+    # plot_recontructed_phi(k_term_aprox, ks, "Hadamard basis")
+    # plot_MSE(k_term_aprox, ks, "Hadamard basis")
+
+    # hadamard = Hadamard(2)
+    # walsh_hadamard = WalshHadamard(hadamard)
+    # walsh_hadamard_basis = create_basis(walsh_hadamard, func_range, 4)
+    # k_term_aprox = KTermApproximator(phi, phi_pre, walsh_hadamard_basis, 4)
+    # k_term_aprox.approximate(4)
+    # k_term_aprox.reconstruct_phi()
+    # plot_recontructed_phi(k_term_aprox, ks, "Walsh-Hadamard basis")
+    # plot_MSE(k_term_aprox, ks, "Walsh-Hadamard basis")
+
+    haar = Haar(2)
+    haar_basis = create_basis(haar, func_range, 4)
+    k_term_aprox = KTermApproximator(phi, phi_pre, haar_basis, 4)
+    k_term_aprox.approximate(4)
+    k_term_aprox.reconstruct_phi()
+    plot_recontructed_phi(k_term_aprox, ks, "haar basis")
+    plot_MSE(k_term_aprox, ks, "haar")
